b798b7ee862f70d1af51bfd1b3d67acafd2c4f8f
Svetlana Arutsiunova <ys20051980@gmail.com>
2023-03-11 18:10:51 +0300

    add homework6

---
+++ homeworks/homework6/Program.cs
@@ -0,0 +1,49 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+// void PositivNumber (int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0)
+//         {
+//             count++;
+//         }
+
+//     }
+//     Console.WriteLine($"Кол-во элементов > 0: {count}");
+// }
+
+
+// Console.Write("Введите через пробел числа: ");
+// int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// PositivNumber (array);
+
+
+
+
+//Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.{
+
+
+
+// Console.WriteLine("введите значение b1");
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число k1");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите значение b2");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число k2");        
+// double k2 = Convert.ToInt32(Console.ReadLine());
+
+// double x = (-b2 + b1)/(-k1 + k2);
+// double y = k2 * x + b2;
+
+
+// Console.WriteLine($"Прямые пересекутся в точке ({x}, {y})");
+
